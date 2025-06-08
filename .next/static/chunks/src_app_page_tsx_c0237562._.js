cc3693817143699b8cd5aeb8b33d8d44b09b0b68
futuretechgenius1 <manoharyadavit@gmail.com>
2025-06-08 20:59:57 +0530

    Add .next/static/chunks/src_app_page_tsx_c0237562._.js 1749396598144

---
+++ .next/static/chunks/src_app_page_tsx_c0237562._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_a8b4c1a9._.js",
+  "static/chunks/node_modules_813df9b4._.js"
+],
+    source: "dynamic"
+});
